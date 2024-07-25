931a7a34d5174df999e27ab76cf1984eb9d4457a
Chatchawan <chatchawant03@gmail.com>
2024-07-25 14:25:58 +0700

    select data

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,14 +1,27 @@
 //src/app/api/route.js
 //-------------------------------------------------------------------------------------
+import { NextResponse } from 'next/server';
+import { Client } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+client.connect();
+
 export async function GET() {
     try {
-      return new Response(JSON.stringify({ message: "GET DATA OK"}), {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
+        const result = await client.query('SELECT * FROM tbl_user');
+        return new Response(JSON.stringify(result.rows), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
       });
     } catch (error) {
       //console.error(error);
-      return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      return new Response(JSON.stringify({ error: error }), {
         status: 500,
         headers: { "Content-Type": "application/json" },
       });
