08493bde0574f1289c53e6ce01e41cf7547fa930
Chatchawan <chatchawant03@gmail.com>
2024-08-29 19:19:38 +0700

    edit CROS and Order user

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -15,7 +15,7 @@ client.connect();
 
 export async function GET() {
   try {
-    const result = await client.query("SELECT * FROM tbl_user");
+    const result = await client.query("SELECT * FROM tbl_user ORDER BY id ASC");
     return new Response(JSON.stringify(result.rows), {
       status: 200,
       headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
@@ -41,50 +41,51 @@ export async function POST(request) {
     );
     return new Response(JSON.stringify(res.rows[0]), {
       status: 201,
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
     });
   } catch (error) {
     console.error(error);
     return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
     });
   }
 }
+
 //-------------------------------------------------------------------------------------
 export async function PUT(request) {
   try {
-    const { id, firstname, lastname, password } = await request.json();
+    const { user_id, firstname, lastname, password } = await request.json();
     const hashedPassword = await bcrypt.hash(password, 10);
     const res = await client.query(
       "UPDATE tbl_user SET firstname = $1, lastname = $2, password = $3 WHERE id = $4 RETURNING *",
-      [firstname, lastname, hashedPassword, id]
+      [firstname, lastname, hashedPassword, user_id]
     );
     if (res.rows.length === 0) {
       return new Response(JSON.stringify({ error: "User not found" }), {
         status: 404,
-        headers: { "Content-Type": "application/json" },
+        headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
       });
     }
     return new Response(JSON.stringify(res.rows[0]), {
       status: 200,
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
     });
   } catch (error) {
     console.error(error);
     return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
     });
   }
 }
 //-------------------------------------------------------------------------------------
 export async function DELETE(request) {
   try {
-    const { id } = await request.json();
+    const { user_id } = await request.json();
     const res = await client.query(
       "DELETE FROM tbl_user WHERE id = $1 RETURNING *",
-      [id]
+      [user_id]
     );
     if (res.rows.length === 0) {
       return new Response(JSON.stringify({ error: "User not found" }), {
